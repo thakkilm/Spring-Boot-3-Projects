bbefabaa87e7219c5a1ea439660fdebd3d5f9766
Mahesh <thakkilm@mail.gvsu.edu>
2023-12-02 13:32:42 -0500

    create profects for spring demo, actuator, security

---
+++ 02-spring-boot-actuator/src/main/java/com/mahesh/springboot/demo/myFirstSpring/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.mahesh.springboot.demo.myFirstSpring.rest;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+//    @Value("${my.name}")
+//    public String name;
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+//        return name;
+    }   
+}
